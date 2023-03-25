f3530f8a149795ff6b1e2b3ed8dc0b62d1d0dd58
LightX2417 <113821789+LightX2417@users.noreply.github.com>
2023-03-25 14:59:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-let myLinks = []
+let myLinks = {}
 const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
 const tabBtn = document.getElementById("tab-btn")
 const deleteBtn = document.getElementById("delete-btn")
 
 let ulEl = document.getElementById("ul-el")
-
-const tabs = [
-    {url: "https://www.youtube.com/"}
-]
 
 const linksFromLocalStorage = JSON.parse(localStorage.getItem("myLinks"))
 
@@ -19,35 +14,49 @@ if (linksFromLocalStorage) {
 
 deleteBtn.addEventListener("dblclick", ()=> {
     localStorage.removeItem("myLinks");
-    myLinks = [];
+    myLinks = {};
     renderLinks(myLinks);
 })
 
 tabBtn.addEventListener("click", ()=> {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs)=>{
-        myLinks.push(tabs[0].url)
+        if(inputEl.value) {
+            myLinks[inputEl.value] = tabs[0].url;
+            inputEl.value = null;
+        } else {
+            myLinks[tabs[0].title] = tabs[0].url;
+        }
         localStorage.setItem("myLinks",JSON.stringify(myLinks))
         renderLinks(myLinks)
     })
 })
 
-inputBtn.addEventListener("click", ()=> {
-    myLinks.push(inputEl.value)
-    inputEl.value = null
-    localStorage.setItem("myLinks",JSON.stringify(myLinks))
-    renderLinks(myLinks)
-})
-
 function renderLinks(links) {
     let listitems = ""
-    for(let i = 0; i<links.length; i++) {
+    for(let key in links) {
         listitems += `
             <li>
-                <a href='${links[i]}' target='_blank'>
-                    ${links[i]}
+                <a href='${links[key]}' target='_blank'>
+                    ${key}
                 </a>
+                <button class='delete-item-btn' data-key='${key}'>delete</button>
             </li>
         `
     }
     ulEl.innerHTML = listitems
+    
+    const deleteItemBtns = document.querySelectorAll(".delete-item-btn");
+    for (let i = 0; i < deleteItemBtns.length; i++) {
+        deleteItemBtns[i].addEventListener("click", (event) => {
+            const key = event.target.getAttribute("data-key");
+            deleteItem(key);
+        });
+    }
+}
+
+function deleteItem(key) {
+    delete myLinks[key];
+    localStorage.removeItem("myLinks")
+    localStorage.setItem("myLinks",JSON.stringify(myLinks))
+    renderLinks(myLinks)
 }
